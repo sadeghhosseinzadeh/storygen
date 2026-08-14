15936f7488b24f6234b700fe3f6f8dd6589add0e
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-08-14 20:17:08 +0330

    Add new template file template_1a.py

--- storygen/templates/template_1a.py
+++ storygen/templates/template_1a.py
@@ -3,7 +3,7 @@ import random
 from PIL import ImageFilter
 from storygen.utils import lighten_color, load_font
 
-def template_1(photo_1, main_color, second_color, model_name, sizes):
+def template_1a(photo_1, main_color, second_color, model_name, sizes):
     shoe_img = photo_1
     W, H = 1080, 1920
 
