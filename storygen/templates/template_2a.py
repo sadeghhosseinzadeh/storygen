f02b78d6b79cfce5918d0bf3829f97e1de1edaeb
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-08-15 11:47:11 +0330

    Update template_2a.py

--- storygen/templates/template_2a.py
+++ storygen/templates/template_2a.py
@@ -7,7 +7,7 @@ from storygen.utils import (
     draw_trapezoid, add_user_logo, draw_text
 )
 
-def template_2a(photo_1, photo_2, model_name, shop_name_en, sizes, brand, username):
+def template_2a(photo_1, photo_2, model_name, shop_name_en, sizes, brand, logo=None):
     W, H = 1080, 1920
 
     # Remove background and get colors
