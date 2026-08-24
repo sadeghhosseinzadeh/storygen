e4f42c61f588a73f2b776de66c26d2694a31d02c
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-08-24 14:45:41 +0330

    Update template_1b.py

--- storygen/templates/template_1b.py
+++ storygen/templates/template_1b.py
@@ -17,7 +17,7 @@ def template_1b(photo_1, model_name, sizes, shop_name_en, brand):
 
     font_mid = load_font("Segoe.UI_p30download.com.ttf", 35)
     font_fid = load_font("Homa.ttf", 38)
-    font_bid = load_font("Segoe.UI_p30download.com.ttf", 55)
+    font_bid = load_font("Segoe.UI.Bold_p30download.com.ttf", 55)
     font_brand = load_font("Segoe.UI.Semibold_p30download.com.ttf", 55)
 
     text_color = main_color
