9aed388aa7dc35974b7a3a4f41f0796be184d517
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-09-11 13:07:19 +0330

    Update template_1g.py

--- storygen/templates/template_1g.py
+++ storygen/templates/template_1g.py
@@ -218,7 +218,7 @@ def template_1g(photo_1, model_name, sizes, shop_name_en, brand, logo):
         font_path="fx-neofara-black-italic.otf",
         max_font_size=550,
         max_width= 1000,
-        start_pos=(100, 325),
+        pos=(100, 325),
         fill=protect_co,
         rotation=0
     )
