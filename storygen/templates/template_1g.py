aa4781bb8d8ca565e91cfe7f84955532fe2ae062
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-09-12 21:14:12 +0330

    Update template_1g.py

--- storygen/templates/template_1g.py
+++ storygen/templates/template_1g.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 import cairo
 import random
 import numpy as np
+from storygen.utils.sizes_utils import draw_sizes_grid
 
 from storygen.utils import (
     lighten_color,
