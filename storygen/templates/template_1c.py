0c421d7d8ba986034d1be582cb840190fe7ddae6
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-09-14 18:36:11 +0330

    Update template_1c.py

--- storygen/templates/template_1c.py
+++ storygen/templates/template_1c.py
@@ -169,12 +169,13 @@ def template_1c(photo_1, model_name, sizes, shop_name_en, brand, logo):
         angle_right=-40,
         center_x=True,
     
-        shadow_scale=1.5,
+        shadow_scale=1.7,
         shadow_rotation=0,
         shadow_opacity=0.6,      # 70% opacity
-        shadow_offset=(0, 0),   # global offset
+        shadow_offset=(0, -30),   # global offset
         toe_offset=(0, -5),      # fine adjustment around toe
-        flip_shadow_for_left=True
+        flip_shadow_for_left=True,
+        use_darken_blend=True
     )
 
 
