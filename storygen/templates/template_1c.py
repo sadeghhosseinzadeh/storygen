1d4188312e061ca00bd3607b8631fb3cae23a5e7
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-09-14 19:08:10 +0330

    Update template_1c.py

--- storygen/templates/template_1c.py
+++ storygen/templates/template_1c.py
@@ -164,12 +164,12 @@ def template_1c(photo_1, model_name, sizes, shop_name_en, brand, logo):
         angle_right=-40,
         center_x=True,
     
-        shadow_front_width=140,   # thicker under toe
-        shadow_back_width=35,     # thinner under heel
-        shadow_length=0.50,       # shorter shadow
-        shadow_darkness=170,      # darker
-        shadow_softness=50,       # softer blur
-        shadow_offset=(0, 15),    # move shadow down a bit
+        shadow_front_width=100,   # thicker under toe
+        shadow_back_width=50,     # thinner under heel
+        shadow_length=1,       # shorter shadow
+        shadow_darkness=190,      # darker
+        shadow_softness=30,       # softer blur
+        shadow_offset=(0, 5),    # move shadow down a bit
     )
 
 
