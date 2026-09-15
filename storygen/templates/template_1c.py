b18408d203d3d2edf5de77f1b23fbb3bfbe927d1
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-09-15 14:04:47 +0330

    Update template_1c.py

--- storygen/templates/template_1c.py
+++ storygen/templates/template_1c.py
@@ -176,11 +176,8 @@ def template_1c(photo_1, model_name, sizes, shop_name_en, brand, logo):
         toe_offset=(-10, -5),      # fine adjustment around toe
         flip_shadow_for_left=True,
         shadow_blend_mode="multiply",
-        shadow_feather=2,  
+        shadow_feather=0,  
     )
-
-
-
 
 
     # -------------------------
