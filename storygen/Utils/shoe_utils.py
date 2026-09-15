46c020f2a3b16e266b98d90fc044f8e97939dd61
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-09-15 22:19:35 +0330

    Update shoe_utils.py

--- storygen/Utils/shoe_utils.py
+++ storygen/Utils/shoe_utils.py
@@ -89,7 +89,7 @@ def place_shoe_1c(
         mask_arr = np.array(mask).astype(np.float32) / 255.0
 
         # --- Threshold clipping (fixes white edge) ---
-        mask_arr[mask_arr < 0.05] = 0
+        mask_arr[mask_arr < 0.02] = 0
 
         # Apply opacity
         mask_arr *= shadow_opacity
