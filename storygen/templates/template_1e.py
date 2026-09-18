f1822e1934b4cb0f7df25249407015b3fe86e5da
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-09-18 18:47:27 +0330

    Update and rename template_1dd.py to template_1e.py

--- storygen/templates/template_1e.py
+++ storygen/templates/template_1e.py
@@ -109,7 +109,7 @@ def draw_circle(
 
     return canvas
 
-def template_1d(photo_1, model_name, sizes, shop_name_en, brand, logo):
+def template_1e(photo_1, model_name, sizes, shop_name_en, brand, logo):
     W, H = 1080, 1920
 
     # -------------------------
@@ -214,7 +214,7 @@ def template_1d(photo_1, model_name, sizes, shop_name_en, brand, logo):
     # Shoe
     # -------------------------
     package_root = Path(storygen.__file__).parent
-    shadow_path = package_root / "bg" / "template1d_shadow2.png"
+    shadow_path = package_root / "bg" / "template1e_shadow2.png"
     shadow_png = Image.open(shadow_path)
     
     place_shoe_1c(
