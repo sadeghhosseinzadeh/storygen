bb2b8421712dca5a141608405430a9b1c45ebcd7
sadeghhosseinzadeh <161209884+sadeghhosseinzadeh@users.noreply.github.com>
2026-09-22 12:24:48 +0330

    Update template_1a.py

--- storygen/templates/template_1a.py
+++ storygen/templates/template_1a.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import storygen
 
 from storygen.utils import (
+    reshape_persian,
     lighten_color,
     load_font,
     remove_background,
@@ -205,7 +206,7 @@ def template_1a(photo_1, model_name, shop_name_en, sizes, brand, logo=None):
 
     # --- 8. Footer text ---
     rand_num = random.randint(100, 999)
-    footer_main = "استعلام قیمت عدد"
+    footer_main = reshape_persian("استعلام قیمت عدد")
     footer_number = f"({to_english_digits(str(rand_num))})"
     
     base_x = 400
